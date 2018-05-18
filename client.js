1047e63efddeb575e0b9e8070597bb69aa74f85b
srz1992 <zilka.sr@gmail.com>
2018-05-18 18:22:39 -0500

    inputs are stored and then displayed onto DOM, then input fields cleared

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 console.log('js');
 
 let employees = [];
+let monthlyTotal = 0;
 
 class Employee {
     constructor (firstName,lastName,id,title,annualSalary) {
@@ -16,17 +17,44 @@ $(document).ready(readyNow);
 
 function readyNow(){
     $('#addButton').on('click', appendToTable)
-
-}
-
-function appendToTable() {
-    console.log('this button works');
-    let newEmployee = new Employee ($('#firstNameIn').val(),$('#lastNameIn').val(),$('#idIn').val(),$('#titleIn').val(),$('#annualSalaryIn').val());
-    employees.push(newEmployee);
-    console.log(employees);   
-}
-
-function displayTable(){
+    $('#addButton').on('click', clearInputs)
+    displayTableHeader();
     
 }
 
+function appendToTable() {
+    let newEmployee = new Employee ($('#firstNameIn').val(),$('#lastNameIn').val(),$('#idIn').val(),$('#titleIn').val(),$('#annualSalaryIn').val());
+    employees.push(newEmployee);
+    console.log(employees);   
+    displayTable();
+}
+
+function displayTableHeader(){
+    let displayHeader = $('#tableOutput')
+    headerOutput = `<tr><th scope="col" >First Name</th>`;
+    headerOutput+= `<th scope="col">Last Name</th>`;
+    headerOutput+= `<th scope="col">ID</th>`;
+    headerOutput+= `<th scope="col">Title</th>`;
+    headerOutput+= `<th scope="col">Annual Salary</th></tr>`;
+    displayHeader.append(headerOutput);
+    
+}
+
+function displayTable(){
+    let display = $('#tableOutput');
+    display.empty();
+    displayTableHeader();
+    for (employee of employees) {
+        let outputString = '<tr><td>' + employee.firstName + '</td>';
+        outputString+= '<td>' + employee.lastName + '</td>';
+        outputString+= '<td>' + employee.id + '</td>';
+        outputString+= '<td>' + employee.title + '</td>';
+        outputString+= '<td>' + employee.annualSalary + '</td>';
+        outputString+= '</tr>';
+        display.append(outputString);
+    }
+}
+
+function clearInputs(){
+    $('.reset').val('');
+}
