faaae947684f5e6fecdc8af43eaec397ca31bf26
sean zilka <zilka.sr@gmail.com>
2018-05-20 14:45:16 -0500

    delete button deletes row and then removes appropriate array index

--- client.js
+++ client.js
@@ -3,7 +3,10 @@ console.log('js');
 let employees = [];
 let costs= 0;
 
+
+
 class Employee {
+    //create employee constructor
     constructor (firstName,lastName,id,title,annualSalary) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,13 +19,18 @@ class Employee {
 $(document).ready(readyNow);
 
 function readyNow(){
+   //give submit button event listeners to run functions on click
     $('#addButton').on('click', appendToTable)
     $('#addButton').on('click', clearInputs)
     $('#addButton').on('click', calculateCosts)
+    //display table header when document is ready
     displayTableHeader();
+    //display monthly costs when document is ready
     displayMonthlyCosts();
+
 }
 
+//pushes new employee object into array and appends new employee to table by running displayTable()
 function appendToTable() {
     let newEmployee = new Employee ($('#firstNameIn').val(),$('#lastNameIn').val(),$('#idIn').val(),$('#titleIn').val(),$('#annualSalaryIn').val());
     employees.push(newEmployee);
@@ -30,50 +38,66 @@ function appendToTable() {
     displayTable();
 }
 
+//displays table header
 function displayTableHeader(){
     let displayHeader = $('#tableOutput')
     headerOutput = `<tr><th scope="col" >First Name</th>`;
     headerOutput+= `<th scope="col">Last Name</th>`;
     headerOutput+= `<th scope="col">ID</th>`;
     headerOutput+= `<th scope="col">Title</th>`;
-    headerOutput+= `<th scope="col">Annual Salary</th></tr>`;
+    headerOutput+= `<th scope="col">Annual Salary</th>`;
+    headerOutput+= `<th scope="col"></th></tr>`
     displayHeader.append(headerOutput);
     
 }
 
+//displays table. empties the table each time appendToTable is run, and then loops through
+//the employees array so the new employees are always reflected in the table.
 function displayTable(){
     let display = $('#tableOutput');
     display.empty();
     displayTableHeader();
+    let identifier = 0;
     for (employee of employees) {
-        let outputString = '<tr><td>' + employee.firstName + '</td>';
+        let outputString = '<tr class="removeable"><td>' + employee.firstName + '</td>';
         outputString+= '<td>' + employee.lastName + '</td>';
-        outputString+= '<td>' + employee.id + '</td>';
+        outputString+= '<td class="deleteInput">' + employee.id + '</td>';
         outputString+= '<td>' + employee.title + '</td>';
         outputString+= '<td>$' + employee.annualSalary + '</td>';
-        outputString+= '</tr>';
-        display.append(outputString);
+        outputString+= '<td><button class="removeRow"id='+'"' +identifier+'"'+'>Delete</button></td>';
+        outputString+= '</tr>';        
+        display.append(outputString);        
+        identifier++;        
+        
     }
+
+    //adds event listener to submit button to delete button in each row
+    $('.removeRow').on('click', removeTableRow)
 }
 
+//clears input fields
 function clearInputs(){
     $('.reset').val('');
 }
 
+//calculates the sum of all annual salaries
 function calculateCosts(){
+    costs = 0;
     for (employee of employees) {
         costs+= parseInt(employee.annualSalary);
-        console.log(costs);
+        // console.log(costs);
     }
-    displayMonthlyCosts()
+    displayMonthlyCosts();
 }
 
+//appends cost variable onto the DOM
 function displayMonthlyCosts (){
     $('#monthlyCosts').empty();
     $('#monthlyCosts').append('<p> Monthly Costs: ' + costs + '</p>');
     theCostsAreTooDamnHigh();
 }
 
+//assigns .cost-div css background color of red when the costs go over 20000.
 function theCostsAreTooDamnHigh(){
     if (costs > 20000){
     $('.cost-div').css("background-color", "red");
@@ -81,3 +105,22 @@ function theCostsAreTooDamnHigh(){
     
     }
 }
+
+//removes table row
+//should also delete the row's index from the array. working on that
+function removeTableRow(){
+    $(this).closest('tr').remove();
+    let employeeIdentifier= Number($(this).attr('id'));
+    console.log(employeeIdentifier);
+    for (let employee = 0; employee<employees.length; employee++) {                
+        if (employee==employeeIdentifier) {
+            console.log('employee at employees['+employee+'] was deleted');
+            
+            employees.splice(employee, 1)
+        }
+    }
+    displayTable();
+   
+}
+ 
+
